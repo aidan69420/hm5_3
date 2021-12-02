02b96fcc8ce5af8f2d2755243c6ac2bc3c27596d
Aidar <aidar.azhibraimov@mail.ru>
2021-12-02 23:40:39 +0600

    upgrade code

--- app/src/main/java/com/geektech/month3_android1_homework5/MainFragment.java
+++ app/src/main/java/com/geektech/month3_android1_homework5/MainFragment.java
@@ -10,8 +10,6 @@ import android.view.ViewGroup;
 
 public class MainFragment extends Fragment {
 
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
