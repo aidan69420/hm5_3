7e32c8eeaa57af1e5c059a710772f397f654065d
Aidar <aidar.azhibraimov@mail.ru>
2021-12-02 23:40:39 +0600

    upgrade code

--- app/src/main/java/com/geektech/month3_android1_homework5/MainActivity.java
+++ app/src/main/java/com/geektech/month3_android1_homework5/MainActivity.java
@@ -8,21 +8,15 @@ import android.os.Bundle;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private RecyclerAdapter recyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = findViewById(R.id.recycler);
         getSupportFragmentManager().beginTransaction().add(R.id.container, new MainFragment()).commit();
         getSupportFragmentManager().beginTransaction().add(R.id.container2, new SecondFragment()).commit();
-        ArrayList<String> list = new ArrayList<>();
 
-            list.add("Blank Space");
 
-        recyclerAdapter = new RecyclerAdapter(list);
-        recyclerView.setAdapter(recyclerAdapter);
+
     }
 }
--- app/src/main/java/com/geektech/month3_android1_homework5/RecyclerAdapter.java
+++ app/src/main/java/com/geektech/month3_android1_homework5/RecyclerAdapter.java
@@ -3,18 +3,24 @@ package com.geektech.month3_android1_homework5;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class RecyclerAdapter extends RecyclerView.Adapter<ViewHolder> {
-    ArrayList<String> list = new ArrayList<>();
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+    private ArrayList<Model> list;
+    public Listener listener;
 
-    public RecyclerAdapter(ArrayList<String> list) {
-        this.list = list;
+    public void setListener (ArrayList<Model> models, Listener listener) {
+        this.list = models;
+        this.listener = listener;
+        this.list.addAll(list);
+        notifyDataSetChanged();
     }
+
 
     @NonNull
     @Override
@@ -32,5 +38,27 @@ public class RecyclerAdapter extends RecyclerView.Adapter<ViewHolder> {
     @Override
     public int getItemCount() {
         return list.size();
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView musicExecutor;
+        private TextView musicName;
+        private TextView numbers;
+        private TextView numbersMusic;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            musicExecutor = itemView.findViewById(R.id.musicExecutor);
+            musicName = itemView.findViewById(R.id.musicName);
+            numbers = itemView.findViewById(R.id.numbers);
+            numbersMusic = itemView.findViewById(R.id.numbersMusic);
+        }
+        public void onBind(Model title){
+            this.musicExecutor.setText(title.getMusicExecutor());
+            this.musicName.setText(title.getMusicName());
+            this.numbers.setText(title.getNumbers());
+            this.numbersMusic.setText(title.getNumbersMusic());
+            itemView.setOnClickListener(v -> {
+                listener.onClick(title);
+            });
+        }
     }
 }
